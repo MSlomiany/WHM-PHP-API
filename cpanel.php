44dfddcaaabe30209658f2a3da07f96d35481553
Michał Słomiany <m.slomiany@outlook.com>
2020-07-16 16:30:30 +0200

    new features

--- cpanel.php
+++ cpanel.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * \class cpanel
  * \brief This is class which allow to perform 
@@ -6,10 +7,14 @@
  */
 class cpanel
 {
+    /**
+     * class private variables
+     * username & password for WHM panel
+     * server IP
+     */
     private $username;  //login
     private $password;  //hasło     
     private $host;      //serwer
-
     /**
      * class cpanel constructor
      */
@@ -49,42 +54,58 @@ class cpanel
     {
         return $this->host;
     }
-
-    /*
-        Creating new account
-        Parse parameters as an associative array
-    */
+    /**
+     * Creating new account
+     * Parse parameters as an associative array
+     */
     public function createAccount($username, $domain, $contactemail, $password, $plan)
     {
-        return $this->runQuery('createacct',[
-            'username'=>$username,
-            'domain'=>$domain,
-            'contactemail'=>$contactemail,
-            'password'=>$password,
-            'plan'=>$plan
+        return $this->executeQuery('createacct', [
+            'username' => $username,
+            'domain' => $domain,
+            'contactemail' => $contactemail,
+            'password' => $password,
+            'plan' => $plan
         ]);
     }
-
-    /*
-        Removing existing account
-    */
+    /**
+     * Removing existing account
+     */
     public function removeAccount($username)
     {
-        return $this->runQuery('removeacct',[
-            'username'=>$username
+        return $this->executeQuery('removeacct', [
+            'username' => $username
         ]);
     }
-
+    /**
+     * Listing account
+     */
+    public function listAccount($username)
+    {
+        return $this->executeQuery('listaccts', [
+            'search' => $username,
+            'searchtype' => 'username'
+        ]);
+    }
+    /**
+     * Request parser
+     */
+    private function createQuery($host, $request, $parameters = [])
+    {
+        $parlist = http_build_query($parameters);
+        return $query = "https://{$host}/json-api/{$request}?api.version=1&{$parlist}";
+    }
     /*
         Request execution by cURL session
+        cURL commands based on WHM API 1 documentation
     */
-    function runQuery($request, $parameters = [])
+    function executeQuery($request, $parameters = [])
     {
         $username = $this->getUsername();
         $password = $this->getPassword();
         $host = $this->getHost();
 
-        $query = "https://127.0.0.1:2087/json-api/listaccts?api.version=1";
+        $query = $this->createQuery($host, $request, $parameters);
 
         $curl = curl_init();                                    // Create Curl Object
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);          // Allow self-signed certs
@@ -97,7 +118,7 @@ class cpanel
         $result = curl_exec($curl);
         if ($result == false) {
             error_log("curl_exec threw error \"" . curl_error($curl) . "\" for $query");    // log error if curl exec fails
-            echo "Invalid request";                             // informacja o błędzie                    
+            echo "Invalid request";                             // error notification                    
         }
         curl_close($curl);
         print $result;
